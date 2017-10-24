1754076f8757d41e58fbf60612b3d7fa54f728a3
Michael SanGiovanni <msangiovanni@snet.net>
2017-10-23 22:41:07 -0400

    Added all progress so far

---
+++ Audio Visualizer/Assets/Scripts/RotateScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour {
+
+	public int rotateSpeed = -100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate((rotateSpeed* Vector3.up) * Time.deltaTime);
+	}
+}
